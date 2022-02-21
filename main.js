c1e9c6c8a0524446da355ad960cb734821e02593
YuiZoldyck <89797588+YuiZoldyck@users.noreply.github.com>
2022-02-21 00:01:39 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -103,3 +103,46 @@ function updateCartTotal() {
     }
     document.getElementsByClassName('cart-total-price')[0].innerText = "THB " + total;
 }
+
+
+const url = "http://api.openweathermap.org/data/2.5/weather?q=" + "Bangkok" + "&units=metric" + "&APPID=d3068e3e914bcf6b7ac1d4454bb8f605";
+fetch(url)
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(json) {
+    let results = "";
+    results = '<div class="weather">'
+    results += '<h4>Weather in ' + json.name + "</h4>";
+    // for (let i = 0; i < json.weather.length; i++) {
+    //     results += '<img class ="center" src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png" width="5vw" height="5vh" />';
+    // }
+    results += '<h4>' + json.main.temp + " &deg;C</h4>"
+    results += '<h4> Feels Like: '
+    results += json.main.feels_like
+    results += ' &deg;C</h4>';
+    results += '<h4> Humidity ' + json.main.humidity + '%</h4>';
+    document.getElementById("weatherResults").innerHTML = results;
+    results += '</div>';
+});
+
+// const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=d3068e3e914bcf6b7ac1d4454bb8f605";
+// fetch(url2)
+//     .then(function(response) {
+//     return response.json();
+//     })
+//     .then(function(json) {
+//     let forecast = '<div class="container">';
+//     for (let i=0; i < 39; i++) {
+//         forecast += '<div class="col">';
+//         forecast += '<h3 class="date">' + moment(json.list[i].dt_txt).format('L') + "</h3>";
+//         forecast += '<h3 class="time">' + moment(json.list[i].dt_txt).format('h:mm:ss a') + "</h3>";
+//         forecast += '<p class="temperature">Temperature: ' + json.list[i].main.temp + " &deg;F</p>";
+//         forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+//         forecast += "<p> Wind: " + json.list[i].wind.speed + "mps</p>";
+//         forecast += '</div>';
+//     }
+//     document.getElementById("forecastResults1").innerHTML = forecast;
+//     forecast = forecast + '</div>';
+
+// });
